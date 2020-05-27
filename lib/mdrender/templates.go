8170f18fe90c6487039212ab0d1cb7294fdaea09
Christian Sakai <christianmsakai@gmail.com>
2020-05-26 23:38:06 -0400

    Applied pkger

--- lib/mdrender/templates.go
+++ lib/mdrender/templates.go
@@ -1,75 +1,49 @@
 package mdrender
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"path"
-	"path/filepath"
-	"runtime"
+
+	"github.com/markbates/pkger"
 )
 
-func getBasePath() (string, bool) {
-	_, b, _, ok := runtime.Caller(0)
-	if !ok {
-		return "", false
-	}
-
-	return filepath.Dir(b), true
-}
-
 func getIndexHtml() (string, error) {
-	basePath, ok := getBasePath()
-	if !ok {
-		return "", errors.New("Failed to get base path")
-	}
-
-	indexHtmlPath := path.Join(basePath, "/templates/index.html")
-
-
-  // indexHtmlBuf, err := pkger.Open("templates/index.html")
-  // if err != nil {
-  //   return "", fmt.Errorf("Failed to get index.html: %w", err)
-  // }
-
-  // indexHtml, err := ioutil.ReadAll(indexHtmlBuf)
-  // if err != nil {
-		// return "", fmt.Errorf("Failed to get index.html: %w", err)
-  // }
-
-	indexHtml, err := ioutil.ReadFile(indexHtmlPath)
+	indexHtmlBuf, err := pkger.Open("/lib/mdrender/templates/index.html")
 	if err != nil {
 		return "", fmt.Errorf("Failed to get index.html: %w", err)
+	}
+
+	indexHtml, err := ioutil.ReadAll(indexHtmlBuf)
+	if err != nil {
+		return "", fmt.Errorf("Failed to read index.html: %w", err)
 	}
 
 	return string(indexHtml), nil
 }
 
 func getWebSocketJs() (string, error) {
-	basePath, ok := getBasePath()
-	if !ok {
-		return "", errors.New("Failed to get base path")
-	}
-
-	webSocketJsPath := path.Join(basePath, "/templates/webSocket.js")
-	webSocketJs, err := ioutil.ReadFile(webSocketJsPath)
+	webSocketJsBuf, err := pkger.Open("/lib/mdrender/templates/webSocket.js")
 	if err != nil {
 		return "", fmt.Errorf("Failed to get websocket script: %w", err)
+	}
+
+	webSocketJs, err := ioutil.ReadAll(webSocketJsBuf)
+	if err != nil {
+		return "", fmt.Errorf("Failed to read websocket script: %w", err)
 	}
 
 	return string(webSocketJs), nil
 }
 
 func getCssSwitchJs() (string, error) {
-	basePath, ok := getBasePath()
-	if !ok {
-		return "", errors.New("Failed to get base path")
-	}
-
-	cssSwitchJsPath := path.Join(basePath, "/templates/cssSwitch.js")
-	cssSwitchJs, err := ioutil.ReadFile(cssSwitchJsPath)
+	cssSwitchJsBuf, err := pkger.Open("/lib/mdrender/templates/cssSwitch.js")
 	if err != nil {
 		return "", fmt.Errorf("Failed to get css switch script: %w", err)
+	}
+
+	cssSwitchJs, err := ioutil.ReadAll(cssSwitchJsBuf)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get read switch script: %w", err)
 	}
 
 	return string(cssSwitchJs), nil
